87a61bbbad31d32b7bfa562f7b98469bf04550cc
Cristian Vintilescu <vintilescu.c.cristian@gmail.com>
2019-08-25 18:09:34 +0200

    main converted to stateful
    MyApp converted to a stateful widget in order to render the updated values in the UI once the user clicks a button

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,52 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  
+    var questionIndex = 0;
+    var questions = [
+      "What's is your favourite color?",
+      "What's is your favourite animal?"
+    ];
+
+    showAnswer(){
+      setState(() {
+      questionIndex = questionIndex + 1;
+      });
+      print('questionIndex: '+questionIndex.toString());
+    }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(title: Text("My first app"),),
-        body:  Center(child:Text("Hello Max!"),),
+        appBar: AppBar(
+          title: Text("My first app"),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              Text(questions[questionIndex]),
+              RaisedButton(
+                child: Text("Answer 1"),
+                onPressed: showAnswer,
+              ),
+              RaisedButton(
+                child: Text("Answer 2"),
+                onPressed: null,
+              ),
+              RaisedButton(
+                child: Text("Answer 3"),
+                onPressed: null,
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
